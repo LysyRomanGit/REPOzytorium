c3de67f184d96908322dfc63658980236433454f
Łysy Roman Git <romanlysygit@lysymail.com>
2025-11-05 17:37:22 +0100

    wykrycie dźwięku

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Kocham skibidi toilet");
+        System.out.println("HOUGH...    PIFPAF");
     }
 }
